8e28df24d24df366a291af64840efd605cc1b609
James Swineson <github@public.swineson.me>
2018-01-16 12:01:03 +0800

    add a pxe server

--- pxe-server/server.py
+++ pxe-server/server.py
@@ -8,7 +8,7 @@ def my_static_dirs():
     return ("release", )
 
 @hug.get("/", output=hug.output_format.text)
-def bootfiles(hug_nocache, signature, timestamp, nonce, **kwargs):
+def bootfiles(**kwargs):
 	with open("release/boot.cfg", "r") as bootcfg:
 		return bootcfg.read()
 
---
+++ server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo uwsgi --http 0.0.0.0:80 --wsgi-file pxe-server/server.py --callable __hug_wsgi__ --master --processes 4 --threads 2
